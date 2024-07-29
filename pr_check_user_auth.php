26c4a602a59e3f173d3eba58b9b02996c5649194
John Anderson <john.anderson@mpdtop.com>
2024-07-29 18:28:53 -0500

    move client side calls to server side
    setup a server side proxy for client side calls to help reduce data visible to client side.

---
+++ pr_check_user_auth.php
@@ -0,0 +1,13 @@
+<?php
+
+	//echo $_SERVER['QUERY_STRING'];
+
+$url = "https://www.widget-list.com/otp-ui/check_for_user_auth.php?".$_SERVER['QUERY_STRING'];
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET"); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+echo curl_exec($ch); 
+
+?>
